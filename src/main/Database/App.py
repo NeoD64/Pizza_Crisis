2ca0364edda324ca64abf83686b811a0d41ffadd
NeoD64 <dewardneo@gmail.com>
2025-09-15 14:40:17 +0200

    app start

--- src/main/Database/App.py
+++ src/main/Database/App.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from model import db
-
+from Model import db
+from flask_sqlalchemy import SQLAlchemy
 def create_app():
     app = Flask(__name__)
 
     # Configure database connection
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:MYsql&$64@localhost:3306/pizza'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:MYsql&$64@localhost:3306/pizza"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Initialize extensions
